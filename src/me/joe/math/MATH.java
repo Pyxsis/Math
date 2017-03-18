d7df64314c2ca4d1695f02454f74802450900edd
Reparo / Joey <joerthecoder@gmail.com>
2017-03-18 13:35:38 +0000

    Annotations
    Annotated code

--- src/me/joe/math/MATH.java
+++ src/me/joe/math/MATH.java
@@ -4,15 +4,14 @@ import me.joe.math.types.Factors;
 
 public class MATH {
 	
-    public static void main(String[] args) {    	
-    	
-    	int optionn = Util.mainMenu();
-    	if(optionn == 1) {
-    		Factors.findFactors();
-    	} else if(optionn == 2) {
-    		System.exit(0);
+    public static void main(String[] args) {    // main place	
+    	int optionn = Util.mainMenu(); // get input
+    	if(optionn == 1) { // if input is 1
+    		Factors.findFactors(); // start finding factors
+    	} else if(optionn == 2) { // if it's 2
+    		System.exit(0); // close the program peacefully
     	} else {
-    		Util.mainMenu();
+    		Util.mainMenu(); // invalid input, send them back to main menu
     	}
     }
     	
