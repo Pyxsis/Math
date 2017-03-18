9038af9488675d048f4da5d65e76267773f9fd32
Reparo / Joey <joerthecoder@gmail.com>
2017-03-18 13:35:18 +0000

    Annotations
    Annotated code

--- src/me/joe/math/Util.java
+++ src/me/joe/math/Util.java
@@ -4,23 +4,23 @@ import javax.swing.JOptionPane;
 
 public class Util {
 	
-	public static double highestNumber(double number1, double number2) {
-		if(number1 > number2) {
-			return number1;
-		} else {
-			return number2;
+	public static double highestNumber(double number1, double number2) { // take input
+		if(number1 > number2) { // if number 1 is over number 2
+			return number1; // number 1 is highest
+		} else { // else
+			return number2; // number 2 is highest
 		}
 	}
 	
-	public static void print(String msg) {
+	public static void print(String msg) { // print dialogue
 		JOptionPane.showMessageDialog(null, msg);
 	}    
     
-	public static int mainMenu() {
+	public static int mainMenu() { // show main menu
     	String optionstr = JOptionPane.showInputDialog("Type one of the following options"
     			+ "\n\"1\" to find common factors of 2 numbers"
-    			+ "\n\"2\" to close");
-    	return Integer.parseInt(optionstr);
+    			+ "\n\"2\" to close"); // get input
+    	return Integer.parseInt(optionstr); // return input
 	}
 	
 	
